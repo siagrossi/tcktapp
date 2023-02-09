0b7f7b6d98e017185ac27c2e3fbbce8c776c3df8
Santiago Iagrossi <santiago.iagrossi@wolterskluwer.com>
2023-02-09 18:34:29 +0100

    feat(functions): add url parameter to generate qr code function

--- libs/netlify-functions/src/lib/generate-qr-code.ts
+++ libs/netlify-functions/src/lib/generate-qr-code.ts
@@ -1,18 +1,32 @@
 import { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
 import QRCode from 'qrcode';
 
+// https://letmegooglethat.com/?q=anda+palla+bobo
+
 const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext
 ) => {
+  if (
+    !event.queryStringParameters ||
+    event.queryStringParameters['url'] == null
+  ) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: `Parameter 'url' missing.`,
+      }),
+    };
+  }
+
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'image/png',
     },
-    body: (
-      await QRCode.toBuffer('https://letmegooglethat.com/?q=anda+palla+bobo')
-    ).toString('base64'),
+    body: (await QRCode.toBuffer(event.queryStringParameters['url'])).toString(
+      'base64'
+    ),
     isBase64Encoded: true,
   };
 };
