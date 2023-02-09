e3f4a275467c16b0456cf87e1bd3696ceddfce3f
Santiago Iagrossi <santiago.iagrossi@wolterskluwer.com>
2023-02-09 19:13:41 +0100

    feat(functions): add pdf generation

--- libs/netlify-functions/src/lib/generate-qr-code.ts
+++ libs/netlify-functions/src/lib/generate-qr-code.ts
@@ -1,5 +1,6 @@
 import { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
 import QRCode from 'qrcode';
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
 
 // https://letmegooglethat.com/?q=anda+palla+bobo
 
@@ -19,14 +20,40 @@ const handler: Handler = async (
     };
   }
 
+  const qrCode = await QRCode.toDataURL(event.queryStringParameters['url']);
+
+  const pdfDoc = await PDFDocument.create();
+
+  const helveticaBoldFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+  const qrImage = await pdfDoc.embedPng(qrCode);
+
+  const page = pdfDoc.addPage();
+
+  const { width, height } = page.getSize();
+
+  const fontSize = 30;
+
+  page.drawText('Here is your ticket for Tu Vieja Fest!', {
+    x: 50,
+    y: height - 4 * fontSize,
+    size: fontSize,
+    font: helveticaBoldFont,
+    color: rgb(0, 0.53, 0.71),
+  });
+
+  page.drawImage(qrImage, {
+    x: width / 2 - 75,
+    y: height / 2 - 75,
+    width: 150,
+    height: 150,
+  });
+
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'image/png',
+      'Content-Type': 'application/pdf',
     },
-    body: (await QRCode.toBuffer(event.queryStringParameters['url'])).toString(
-      'base64'
-    ),
+    body: await pdfDoc.saveAsBase64(),
     isBase64Encoded: true,
   };
 };
